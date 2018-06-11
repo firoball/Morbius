eb06b3b02b54eaf9dd5eb872c99d86a3885f6ff0
firoball <unsinnspala@firoball.de>
2018-06-11 21:29:44 +0200

    Add namespaces

--- Assets/Scripts/Ambient/AnimatedDancer.cs
+++ Assets/Scripts/Ambient/AnimatedDancer.cs
@@ -1,18 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
-public class AnimatedDancer : MonoBehaviour
+namespace Morbius.Scripts.Ambient
 {
-
-    void Start()
+    [RequireComponent(typeof(Animator))]
+    public class AnimatedDancer : MonoBehaviour
     {
-        Animator animator = GetComponent<Animator>();
-        float rnd = Random.Range(-1.0f, 1.0f);
-        bool direction = (Mathf.Sign(rnd) == 1);
-        animator.SetBool("direction", direction);
-        animator.SetFloat("speed", Random.Range(0.8f, 1.2f));
-    }
 
+        void Start()
+        {
+            Animator animator = GetComponent<Animator>();
+            float rnd = Random.Range(-1.0f, 1.0f);
+            bool direction = (Mathf.Sign(rnd) == 1);
+            animator.SetBool("direction", direction);
+            animator.SetFloat("speed", Random.Range(0.8f, 1.2f));
+        }
+
+    }
 }
--- Assets/Scripts/Ambient/RandomColor.cs
+++ Assets/Scripts/Ambient/RandomColor.cs
@@ -1,15 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-[RequireComponent(typeof(Renderer))]
-public class RandomColor : MonoBehaviour
+namespace Morbius.Scripts.Ambient
 {
-
-    void Start()
+    [RequireComponent(typeof(Renderer))]
+    public class RandomColor : MonoBehaviour
     {
-        Renderer renderer = GetComponent<Renderer>();
-        Color color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 0.5f, 1.0f);
-        renderer.material.SetColor("_Color", color);
+
+        void Start()
+        {
+            Renderer renderer = GetComponent<Renderer>();
+            Color color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 0.5f, 1.0f);
+            renderer.material.SetColor("_Color", color);
+        }
     }
 }
